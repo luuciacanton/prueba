2b737351bacf596424f364519aa13fd475a04e3c
luuciacanton <luuciacanton@correo.ugr.es>
2023-03-12 20:41:00 +0100

    holi

---
+++ jejeje.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main (void)
+{
+    int num,i;
+
+    cout<<"Introduzca un numero:";
+    cin>>num;
+
+    for(i=1; i<=num;i++)
+    {
+         cout<<i<<endl;
+    }
+
+    return 0;
+}
